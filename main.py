379ddb9416045e4abe313910675e9b5e2c6c471d
Marek <kabzinskimilosz@gmail.com>
2024-09-15 18:09:03 +0200

    testing api

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import requests
 import asyncio
 import json
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
 
 with open('config.json') as f:
     config = json.load(f)
 
-print(config)
-data = {
-    "key":"TWpBd056SXROVE16TWkweExUUT0",
-    "dataOd":"2024-09-01T22:00:00.000Z",
-    "dataDo":"2024-09-08T21:59:59.999Z",
-    "zakresDanych":1
-}
-headers = {"cookie":""}
+def init():
+    global Session
+    Session = requests.Session()
+    requests.utils.add_dict_to_cookiejar(Session.cookies, {"cookie": config["cookie"]})
+    request_test = Session.get(url=f"https://uczen.eduvulcan.pl/piotrkowtrybunalski/App/TWpBd056SXROVE16TWkweExUUT0/tablica")
+    if request_test.status_code != 200:
+        raise requests.exceptions.ConnectionError("Cookie Expired")
 
-request = requests.get(url="https://uczen.eduvulcan.pl/piotrkowtrybunalski/api/79ad9207-6132-427d-bbfa-62a7cff7735f",params=data,headers=headers)
-print(request.text)
+#print(request.text)
+
+
+@app.route('/plan_zajec', methods=['GET'])
+async def test():
+    weekday_start = datetime.datetime.date(datetime.datetime.now()) - datetime.timedelta(days=datetime.datetime.weekday(datetime.datetime.now()))
+    data = {
+    "key":"TWpBd056SXROVE16TWkweExUUT0",
+    "dataOd":weekday_start.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+    "dataDo":(weekday_start+datetime.timedelta(days=6)).strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+    "zakresDanych":1
+    }
+    
+    headers = {"cookie":config["cookie"]}
+
+    request = Session.get(url=f"{config['api_endpoint']}79ad9207-6132-427d-bbfa-62a7cff7735f",params=data,headers=headers)
+    return request.text,200
+
+
+if __name__ == '__main__':
+    init()
+    app.run(host='0.0.0.0')
